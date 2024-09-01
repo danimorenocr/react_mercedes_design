7182da3ea7b68ef5a8e403af552f54f756820e29
Dani Moreno <danimorenocr@gmail.com>
2024-08-31 20:27:10 -0500

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import miLogo from "./assets/img/logo.jpg";
+import amg from "./assets/img/car.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <div className="logo">
+          <img
+            src={miLogo}
+            alt="logo"
+            className="imagen-logo"
+            style={{ height: "100px" }}
+          />
+        </div>
+        <div className="rectangulo-diagonal"></div>
+        <div>
+          <h1 className="titulo">MERCEDES</h1>
+          <h1 className="titulo benz">BENZ</h1>
+        </div>
+        <div>
+          <img
+            src={amg}
+            alt="amg car"
+            className="amg-car"
+            style={{ height: "450px" }}
+          />
+          <h3 className="marca">AMG-SL</h3>
+        </div>
+        <div className="button-container">
+          <button className="custom-button">VER AUTOS</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
